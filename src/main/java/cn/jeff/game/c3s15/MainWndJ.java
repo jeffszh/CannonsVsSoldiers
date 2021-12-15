013398f388eb8bb19db938bea546b9b9b1447e45
Jeff <zhsu@amwares.com>
2021-12-15 14:50:43 +0800

    试试画图。

--- src/main/java/cn/jeff/game/c3s15/MainWndJ.java
+++ src/main/java/cn/jeff/game/c3s15/MainWndJ.java
@@ -1,7 +1,18 @@
 package cn.jeff.game.c3s15;
 
+import javafx.scene.layout.Pane;
+
 public class MainWndJ {
 
 	MainWnd k;
+	public Pane centerPane;
+
+	public void btn01Click() {
+		k.btn01Click();
+	}
+
+	public void btn02Click() {
+		k.btn02Click();
+	}
 
 }
--- src/main/kotlin/cn/jeff/game/c3s15/MainWnd.kt
+++ src/main/kotlin/cn/jeff/game/c3s15/MainWnd.kt
@@ -1,6 +1,7 @@
 package cn.jeff.game.c3s15
 
 import javafx.fxml.FXMLLoader
+import javafx.scene.canvas.Canvas
 import javafx.scene.layout.BorderPane
 import tornadofx.*
 
@@ -8,6 +9,7 @@ class MainWnd : View(GlobalVars.appConf.mainTitle) {
 
 	override val root: BorderPane
 	private val j: MainWndJ
+	private val chessBoardCanvas: Canvas
 
 	init {
 		primaryStage.isResizable = true
@@ -16,6 +18,28 @@ class MainWnd : View(GlobalVars.appConf.mainTitle) {
 		root = loader.load(javaClass.getResourceAsStream("/cn/jeff/game/c3s15/MainWnd.fxml"))
 		j = loader.getController()
 		j.k = this
+
+		chessBoardCanvas = Canvas(300.0, 200.0)
+		j.centerPane.add(chessBoardCanvas)
+	}
+
+	fun btn01Click() {
+		val gc = chessBoardCanvas.graphicsContext2D
+		gc.fill = c(0, 160, 255)
+		gc.fillRect(0.0, 0.0, chessBoardCanvas.width, chessBoardCanvas.height)
+		gc.fill = c(255, 128, 0)
+		gc.fillRoundRect(80.0, 60.0, 120.0, 90.0, 8.0, 8.0)
+		gc.strokeRoundRect(80.0, 60.0, 120.0, 90.0, 8.0, 8.0)
+//		j.centerPane.clear()
+//		j.centerPane.children.add(canvas)
+//		val pane = Pane()
+//		pane.children.add(canvas)
+		println(j.centerPane.children[0])
+	}
+
+	fun btn02Click() {
+		val gc = chessBoardCanvas.graphicsContext2D
+		gc.clearRect(0.0, 0.0, chessBoardCanvas.width, chessBoardCanvas.height)
 	}
 
 }
