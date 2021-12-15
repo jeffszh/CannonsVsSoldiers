ee7e64c4ce2bd71d528d7d43f1b34c8bebfb54a0
Jeff <zhsu@amwares.com>
2021-12-15 16:38:52 +0800

    试验拖拽操作。

--- src/main/kotlin/cn/jeff/game/c3s15/MainWnd.kt
+++ src/main/kotlin/cn/jeff/game/c3s15/MainWnd.kt
@@ -2,6 +2,9 @@ package cn.jeff.game.c3s15
 
 import javafx.fxml.FXMLLoader
 import javafx.scene.canvas.Canvas
+import javafx.scene.image.Image
+import javafx.scene.input.ClipboardContent
+import javafx.scene.input.TransferMode
 import javafx.scene.layout.BorderPane
 import tornadofx.*
 
@@ -16,11 +19,29 @@ class MainWnd : View(GlobalVars.appConf.mainTitle) {
 
 		val loader = FXMLLoader()
 		root = loader.load(javaClass.getResourceAsStream("/cn/jeff/game/c3s15/MainWnd.fxml"))
+		val img = Image(javaClass.getResourceAsStream("/image/block.png"))
 		j = loader.getController()
 		j.k = this
 
 		chessBoardCanvas = Canvas(300.0, 200.0)
 		j.centerPane.add(chessBoardCanvas)
+
+		j.centerPane.setOnDragDetected {
+			val drg = j.centerPane.startDragAndDrop(TransferMode.MOVE)
+			drg.setDragView(img, 24.0, 24.0)
+			// 坑！必须设置内容，而且内容不为空，才能真正发起拖拽，否则没反应！
+			drg.setContent(ClipboardContent().apply {
+				putString("AbCd")
+			})
+			println("start drag")
+		}
+		j.centerPane.setOnDragEntered { e ->
+			println("enter: ${e.y}, ${e.y}")
+		}
+		j.centerPane.setOnDragOver { e ->
+			println("drag: ${e.x}, ${e.y}")
+			chessBoardCanvas.relocate(e.x, e.y)
+		}
 	}
 
 	fun btn01Click() {
