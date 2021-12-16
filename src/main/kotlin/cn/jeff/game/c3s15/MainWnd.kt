0e50ccd2110aff11782a0ef8e09745fd8c107e46
Jeff <zhsu@amwares.com>
2021-12-16 15:53:36 +0800

    画出棋盘。

--- src/main/kotlin/cn/jeff/game/c3s15/MainWnd.kt
+++ src/main/kotlin/cn/jeff/game/c3s15/MainWnd.kt
@@ -1,95 +1,27 @@
 package cn.jeff.game.c3s15
 
-import cn.jeff.game.c3s15.board.ChessCell
 import javafx.fxml.FXMLLoader
-import javafx.scene.canvas.Canvas
-import javafx.scene.image.WritableImage
-import javafx.scene.input.ClipboardContent
-import javafx.scene.input.TransferMode
 import javafx.scene.layout.BorderPane
-import javafx.scene.text.Font
 import tornadofx.*
-import kotlin.math.abs
 
 class MainWnd : View(GlobalVars.appConf.mainTitle) {
 
 	override val root: BorderPane
 	private val j: MainWndJ
-	private val chessBoardCanvas: ChessCell
 
 	init {
 		primaryStage.isResizable = true
 
 		val loader = FXMLLoader()
 		root = loader.load(javaClass.getResourceAsStream("/cn/jeff/game/c3s15/MainWnd.fxml"))
-		// val img = Image(javaClass.getResourceAsStream("/image/block.png"))
-		val img = Canvas(80.0, 80.0).apply {
-			val gc = graphicsContext2D
-			isMouseTransparent = true
-			gc.clearRect(0.0, 0.0, 80.0, 80.0)
-			gc.fill = c(160, 160, 2)
-			gc.fillRoundRect(2.0, 2.0, 76.0, 76.0, 82.0, 82.0)
-			gc.strokeRoundRect(2.0, 2.0, 76.0, 76.0, 82.0, 82.0)
-			gc.font = Font.font(50.0)
-			gc.strokeText(GlobalVars.appConf.cannonText, 20.0, 60.0)
-		}.snapshot(null, WritableImage(80, 80))
 		j = loader.getController()
 		j.k = this
-		chessBoardCanvas = j.chessBoardCanvas
-		chessBoardCanvas.width = 300.0
-		chessBoardCanvas.height = 200.0
-
-		var mx = 0.0
-		var my = 0.0
-		j.centerPane.setOnDragDetected { e ->
-			val drg = j.centerPane.startDragAndDrop(TransferMode.MOVE)
-			drg.setDragView(img, 24.0, 24.0)
-			// 坑！必须设置内容，而且内容不为空，才能真正发起拖拽，否则没反应！
-			drg.setContent(ClipboardContent().apply {
-				putString("AbCd")
-			})
-			mx = e.x
-			my = e.y
-			println("start drag")
-		}
-		j.centerPane.setOnDragEntered { e ->
-			println("enter: ${e.y}, ${e.y}")
-		}
-		j.centerPane.setOnDragOver { e ->
-			println("drag: ${e.x}, ${e.y}")
-			chessBoardCanvas.relocate(e.x, e.y)
-			if (abs(mx - e.x) > 100 || abs(my - e.y) > 100) {
-				e.acceptTransferModes(TransferMode.MOVE)
-			}
-		}
-		j.centerPane.setOnDragDropped { e ->
-			e.isDropCompleted = true
-			e.consume()
-			println("drop! ${e.x}, ${e.y}")
-		}
-		j.centerPane.setOnDragDone { e ->
-			e.consume()
-			println("done! ${e.x}, ${e.y}")
-		}
 	}
 
 	fun btn01Click() {
-		val gc = chessBoardCanvas.graphicsContext2D
-		gc.fill = c(0, 160, 255)
-		gc.fillRect(0.0, 0.0, chessBoardCanvas.width, chessBoardCanvas.height)
-		gc.fill = c(255, 128, 0)
-		gc.fillRoundRect(-80.0, 60.0, 120.0, 90.0, 8.0, 8.0)
-		gc.strokeRoundRect(-80.0, 60.0, 120.0, 90.0, 8.0, 8.0)
-//		j.centerPane.clear()
-//		j.centerPane.children.add(canvas)
-//		val pane = Pane()
-//		pane.children.add(canvas)
-		println(j.centerPane.children[0])
 	}
 
 	fun btn02Click() {
-		val gc = chessBoardCanvas.graphicsContext2D
-		gc.clearRect(0.0, 0.0, chessBoardCanvas.width, chessBoardCanvas.height)
 	}
 
 }
--- src/main/kotlin/cn/jeff/game/c3s15/board/ChessBoard.kt
+++ src/main/kotlin/cn/jeff/game/c3s15/board/ChessBoard.kt
@@ -1,6 +1,80 @@
 package cn.jeff.game.c3s15.board
 
+import javafx.beans.property.SimpleDoubleProperty
+import javafx.scene.canvas.Canvas
 import javafx.scene.layout.Pane
+import javafx.scene.paint.Color
+import tornadofx.*
+import kotlin.math.floor
+import kotlin.math.min
 
+/**
+ * # 棋盘
+ *
+ * 由于棋盘是正方形的，有些东西可以简便处理。
+ */
 class ChessBoard : Pane() {
+
+	companion object {
+		/** 边界留白 */
+		private const val borderPadding = 10.0
+	}
+
+	/** 棋盘格的尺寸 */
+	private val cellSizeProperty = SimpleDoubleProperty(0.0)
+
+	private val backgroundCanvas = Canvas()
+	private var isSizeChanged = false
+
+	init {
+		add(backgroundCanvas)
+		cellSizeProperty.onChange { cellSize ->
+			repaintBackground(cellSize)
+		}
+		widthProperty().onChange {
+			sizeChanged()
+		}
+		heightProperty().onChange {
+			sizeChanged()
+		}
+	}
+
+	private fun sizeChanged() {
+		isSizeChanged = true
+		// 这里运用了延迟运行的技巧，可以使width和height连续改变的时候只触发一次。
+		runLater {
+			if (isSizeChanged) {
+				println("新大小 = $width x $height")
+				cellSizeProperty.value = floor((min(width, height) - borderPadding * 2) / 20) * 4
+				isSizeChanged = false
+			}
+		}
+	}
+
+	private fun repaintBackground(cellSize: Double) {
+		backgroundCanvas.apply {
+			width = cellSize * 5 + borderPadding * 2
+			height = width
+			// 居中
+			layoutX = (this@ChessBoard.width - width) / 2
+			layoutY = (this@ChessBoard.height - height) / 2
+			graphicsContext2D.apply {
+				clearRect(0.0, 0.0, width, height)
+				stroke = Color.BLACK
+				// 画最外面的线，稍粗一点。
+				lineWidth = 3.0
+				val internalWidth = width - 2 * borderPadding
+				strokeRect(borderPadding, borderPadding, internalWidth, internalWidth)
+				// 画里面的线，稍细一点。
+				lineWidth = 2.0
+				for (i in 1..4) {
+					// 正方形，横线和竖线同理，每次循环同时画一条横线和一条竖线。
+					val d = borderPadding + i * cellSize
+					strokeLine(borderPadding, d, width - borderPadding, d)
+					strokeLine(d, borderPadding, d, height - borderPadding)
+				}
+			}
+		}
+	}
+
 }
