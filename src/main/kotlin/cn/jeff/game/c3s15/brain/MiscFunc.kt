18666f97387242a9bdec4c29417268afb0d6d6b5
jeffszh <jeffszh@126.com>
2021-12-17 22:14:27 +0800

    嘗試優化坐標的計算，仍不太好。

--- src/main/kotlin/cn/jeff/game/c3s15/brain/MiscFunc.kt
+++ src/main/kotlin/cn/jeff/game/c3s15/brain/MiscFunc.kt
@@ -8,45 +8,25 @@ fun calcArrowPolygonPoints(
 ): List<Point2D> {
 	val midX = toX - (toX - fromX) * .25
 	val midY = toY - (toY - fromY) * .25
-	val tipX1: Double
-	val tipX2: Double
-	val tipY1: Double
-	val tipY2: Double
-	val midX1: Double
-	val midX2: Double
-	val midY1: Double
-	val midY2: Double
-	val stX1: Double
-	val stX2: Double
-	val stY1: Double
-	val stY2: Double
-	if (fromX == toX) {
-		tipY1 = midY
-		tipY2 = midY
-		tipX1 = midX + d2
-		tipX2 = midX - d2
-		midY1 = midY
-		midY2 = midY
-		midX1 = midX + d1
-		midX2 = midX - d1
-		stY1 = fromY
-		stY2 = fromY
-		stX1 = fromX + d1
-		stX2 = fromX - d1
+
+	// 這段代碼可以用矩陣來優化，懶得慢慢想，用無腦辦法算了。
+	val (d1X, d1Y, d2X, d2Y) = if (fromX == toX) {
+		listOf(d1, .0, d2, .0)
 	} else {
-		tipX1 = midX
-		tipX2 = midX
-		tipY1 = midY + d2
-		tipY2 = midY - d2
-		midX1 = midX
-		midX2 = midX
-		midY1 = midY + d1
-		midY2 = midY - d1
-		stX1 = fromX
-		stX2 = fromX
-		stY1 = fromY + d1
-		stY2 = fromY - d1
+		listOf(.0, d1, .0, d2)
 	}
+	val tipX1 = midX + d2X
+	val tipX2 = midX - d2X
+	val tipY1 = midY + d2Y
+	val tipY2 = midY - d2Y
+	val midX1 = midX + d1X
+	val midX2 = midX - d1X
+	val midY1 = midY + d1Y
+	val midY2 = midY - d1Y
+	val stX1 = fromX + d1X
+	val stX2 = fromX - d1X
+	val stY1 = fromY + d1Y
+	val stY2 = fromY - d1Y
 	return listOf(
 		Point2D(toX, toY),
 		Point2D(tipX1, tipY1),
