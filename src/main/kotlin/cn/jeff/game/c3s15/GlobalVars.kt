81ed322a5d87cbc2a78541e3bf2045f769d7be8f
Jeff <zhsu@amwares.com>
2022-03-28 16:27:29 +0800

    改用YAML做配置文件，但字段顺序必为字母顺序，且无法干预，不好。

--- src/main/kotlin/cn/jeff/game/c3s15/GlobalVars.kt
+++ src/main/kotlin/cn/jeff/game/c3s15/GlobalVars.kt
@@ -4,9 +4,10 @@ import cn.jeff.game.c3s15.board.GameRecord
 import cn.jeff.game.c3s15.brain.PlayerType
 import cn.jeff.game.c3s15.event.NetStatusChangeEvent
 import cn.jeff.game.c3s15.net.BaseNetLink
-import com.google.gson.GsonBuilder
 import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleObjectProperty
+import org.yaml.snakeyaml.DumperOptions
+import org.yaml.snakeyaml.Yaml
 import tornadofx.*
 import java.io.FileReader
 import java.io.FileWriter
@@ -14,14 +15,20 @@ import java.io.IOException
 
 object GlobalVars {
 
-	private const val confFilename = "c3s15.conf.json"
-	private val gson = GsonBuilder().setPrettyPrinting().create()
+	private const val confFilename = "c3s15.conf.yaml"
+	private val yaml = Yaml(DumperOptions().apply {
+		defaultFlowStyle = DumperOptions.FlowStyle.BLOCK
+		lineBreak = DumperOptions.LineBreak.WIN
+	})
+	// private val gson = GsonBuilder().setPrettyPrinting().create()
+
 	var appConf = AppConf()
 
 	fun loadConf(filename: String = confFilename) {
 		try {
 			FileReader(filename).use { reader ->
-				appConf = gson.fromJson(reader, AppConf::class.java)
+				appConf = yaml.loadAs(reader, AppConf::class.java)
+				// appConf = gson.fromJson(reader, AppConf::class.java)
 			}
 		} catch (e: IOException) {
 			e.printStackTrace()
@@ -30,7 +37,8 @@ object GlobalVars {
 
 	fun saveConf(filename: String = confFilename) {
 		FileWriter(filename).use { writer ->
-			gson.toJson(appConf, writer)
+			yaml.dump(appConf, writer)
+			// gson.toJson(appConf, writer)
 		}
 	}
 
