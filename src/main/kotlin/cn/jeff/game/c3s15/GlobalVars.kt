e1e6ebc2b64fc95fb46427f877701237b88336ba
Jeff <zhsu@amwares.com>
2022-03-30 15:24:04 +0800

    准确控制配置文件使用回车换行。

--- src/main/kotlin/cn/jeff/game/c3s15/GlobalVars.kt
+++ src/main/kotlin/cn/jeff/game/c3s15/GlobalVars.kt
@@ -7,6 +7,8 @@ import cn.jeff.game.c3s15.net.BaseNetLink
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleObjectProperty
+import org.yaml.snakeyaml.DumperOptions
+import org.yaml.snakeyaml.Yaml
 import tornadofx.*
 import java.io.FileReader
 import java.io.FileWriter
@@ -17,6 +19,10 @@ object GlobalVars {
 	private const val confFilename = "c3s15.conf.yaml"
 
 	//	private val gson = GsonBuilder().setPrettyPrinting().create()
+	private val yaml = Yaml(DumperOptions().apply {
+		lineBreak = DumperOptions.LineBreak.WIN
+		defaultFlowStyle = DumperOptions.FlowStyle.BLOCK
+	})
 	private val yamlMapper = YAMLMapper.builder().build()
 	private val yamlWriter = yamlMapper.writerWithDefaultPrettyPrinter()
 	private val yamlReader = yamlMapper.reader()
@@ -34,7 +40,14 @@ object GlobalVars {
 
 	fun saveConf(filename: String = confFilename) {
 		FileWriter(filename).use { writer ->
-			yamlWriter.writeValue(writer, appConf)
+//			yamlWriter.writeValue(writer, appConf)
+			// 使用Jackson的writer可确保字段顺序跟类定义的顺序相同（但无法指定换行符格式）。
+			val appConfString = yamlWriter.writeValueAsString(appConf)
+			// SnakeYaml的Yaml对象可以控制换行格式，确保输出的yaml文件一定用回车换行。
+			// 注：Jackson内部其实也是使用SnakeYaml，但不知怎样操控换行符的格式，只好绕个圈来做。
+			// 另：如果直接用SnakeYaml来将appConf写入，则无法操控字段顺序（总是以字母顺序排序，无法定制），
+			// 所以要先用Jackson转为字符串，再用SnakeYaml读入再输出。
+			yaml.dump(yaml.load(appConfString), writer)
 		}
 	}
 
